624c5ebad4c3fdb5eea8b75641bf9d8d046b1d1c
pouryakillsound <pouryakillsound@gmail.coms>
2025-01-25 19:47:17 +0330

    utils: using a better way to show the file format & returning the size of less that 1KB in Bytes

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 import os
 
 def fetch_type(s: str):
-  s = s.rsplit('.')
-  return s[-1] if len(s) > 1 else ''
+  tp = []
+  for c in reversed(s):
+    if c == '.':
+      break
+    tp.append(c)
+  return ''.join(reversed(tp)) if len(s) != len(tp) else 'Undefined'
 
 def fetch_size(address: str):
   size = os.stat(address).st_size
@@ -12,6 +16,8 @@ def fetch_size(address: str):
     return f"{size // (1024 ** 2)} MB"
   elif size > 1024:
     return f"{size // (1024)} KB"
+  else:
+    return f'{size} B'
 
 def fetch_files(dir):
   return [(i.name, fetch_type(i.name), fetch_size(os.path.abspath(dir) + "/" + i.name)) for i in os.scandir(dir) if i.is_file()]
